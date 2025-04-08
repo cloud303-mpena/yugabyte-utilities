b64df2612cd73f9ab8abda204cd712c411a1b732
cloud303-mpena <mpena@cloud303.io>
2025-04-08 14:11:59 -0600

    take first result on YB_RELEASE check

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -8,7 +8,7 @@
 YB_VERSION=$1
 # this query is paginated, so there's no guarantee we will find this release on the first page
 # needs improvement to walk through all pages
-YB_RELEASE=$(curl -s https://registry.hub.docker.com/v2/repositories/yugabytedb/yugabyte/tags |  grep -Eo "${YB_VERSION}-b[0-9]+")
+YB_RELEASE=$(curl -s https://registry.hub.docker.com/v2/repositories/yugabytedb/yugabyte/tags | grep -Eo "${YB_VERSION}-b[0-9]+" | head -n 1)
 YB_HOME=/home/${USER}/yugabyte-db
 YB_DL_BASE="https://downloads.yugabyte.com/releases"
 YB_PACKAGE_URL="${YB_DL_BASE}/${YB_VERSION}/yugabyte-${YB_RELEASE}-linux-x86_64.tar.gz"
